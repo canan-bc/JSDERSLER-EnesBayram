881190e55cc342c5bb834d87d4208816b56ea9ba
Canan <useryedek56@gmail.com>
2025-12-01 15:26:17 +0300

    ilk commit

---
+++ 4.Scope Kavramı/app.js
@@ -0,0 +1,50 @@
+//Var -- Let -- Const
+
+/*
+----- SCOPE---- (KAPSAM)
+-> GLOBAL SCOPE (sınırı olmayan heryerden erişebilen )
+-> FUNCTİON SCOPE
+-> BLOCK SCOPE
+*/
+
+// var degiskenIsmi=10;
+// console.log(degiskenIsmi);
+
+
+
+// var a=5; //global scope
+// if(true){
+// console.log(a);
+// }
+
+// function method1(){
+// console.log(a);
+// }
+// method1();
+
+
+
+// function method1(){  //function scope
+//     var sayi=10;
+//     console.log(sayi);
+// }
+// method1();
+
+
+
+// function method1(){
+//     var a=5; //function scope
+//     if(true){
+//         var a=5; //block scope
+//     }
+//     while(true){
+
+//     }
+//     for(let i=0; i<=10; i++){
+
+//     }
+// }
+
+
+
+
---
+++ 5. Var Let Const/app.js
@@ -0,0 +1,49 @@
+//Var - Let -Const
+
+
+//Değişken nedir?
+/*
+var/let/const =degiskenDegeri;
+let sayi=54;
+*/
+
+
+// function Selamla(){
+//     var selam="Herkese Selamlar"; //function scope
+//     if(true){
+//         let b=47;
+//        console.log(b);
+//     }
+//     console.log(selam);
+// }
+// Selamla();
+
+
+// var a=12;
+// var a=10;
+// console.log(a);  //değişir
+
+
+
+
+
+//NOT: VAR:FUNCTİON SCOPE (function içinde tanımlarsak dışında tanımlarsak global olur)
+//let ve const : block scope özelliğine sahiptir
+
+
+
+//Let ve Const Arsındaki fark nedir?
+//const(constant): sabit,değişmez
+
+// const a=10;
+// a=15;
+// console.log(a); //değişmez
+
+// let b=12;
+// b=85;
+// b=32;
+// console.log(b);  //değişir
+
+
+
+
