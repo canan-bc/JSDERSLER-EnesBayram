0684eb1f24ef5a8eeeacf91414a73e3f49733f8f
Canan <useryedek56@gmail.com>
2025-12-13 13:27:55 +0300

    ilk commit

---
+++ 12.For Döngüsü Pratik/app.js
@@ -0,0 +1,37 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// for(let i=0; i<=10; i+=2){
+//     console.log(i);
+// }
+
+
+
+
+// for(let i=1; i<=10; i+=2){
+//     console.log(i);
+// }
+
+
+
+
+// for(let i=i; i<=10; i++){
+//     if(i%2==1){
+// console.log("Enes")
+//     }else{
+//         console.log("Naber");
+//     }
+// }
+
+
+
+// let toplam=0;
+// for(let i=50; i>=1; i--)
+// {
+//     toplam+=i;
+//     console.log(i);
+// }
+// console.log("Toplam : ",toplam);
